a787b8e1581cb29a96ea451be5fa18c02eb64995
MacBook <MacBook@MacBooks-MBP.home>
2015-07-17 06:12:48 +0100

    Rewrote seeds file for reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ Event.delete_all
 alex = User.create! :name => 'Alex', :bio => 'My Bio',  :email => 'alex@gmail.com', :password => 'password', :password_confirmation => 'password', :profile_image_url =>"alex.jpg"
 harry = User.create! :name => 'Harry', :bio => 'My Bio', :email => 'harry@gmail.com', :password => 'password', :password_confirmation => 'password', :profile_image_url =>"harry.jpg"
 zoe = User.create! :name => 'Zoe', :bio => 'My Bio', :email => 'zoe@gmail.com', :password => 'password', :password_confirmation => 'password', :profile_image_url =>"zoe.jpg"
-grouse = User.create! :name => 'Grouse', :bio => 'My Bio', :email => 'grouse@gmail.com', :password => 'password', :password_confirmation => 'password', :profile_image_url =>"grouse.jpg"
+grouse = User.create! :name => 'Ed', :bio => 'My Bio', :email => 'grouse@gmail.com', :password => 'password', :password_confirmation => 'password', :profile_image_url =>"grouse.jpg"
 remes = User.create! :name => 'Remes', :bio => 'My Bio', :email => 'remes@gmail.com', :password => 'password', :password_confirmation => 'password', :profile_image_url =>"remes.jpg"
 sam = User.create! :name => 'Sam', :bio => 'My Bio', :email => 'sam@gmail.com', :password => 'password', :password_confirmation => 'password', :profile_image_url =>"sam.jpg"
 heloise = User.create! :name => 'Heloise', :bio => 'My Bio', :email => 'heloise@gmail.com', :password => 'password', :password_confirmation => 'password', :profile_image_url =>"heloise.jpg"
@@ -30,50 +30,60 @@ jen = User.create! :name => 'Jen', :bio => 'My Bio', :email => 'jen@gmail.com', 
 # ...........Groups................
 
 alexsgroup = Group.create!(
-  name: 'Alexs Awesome Group',
-  group_description: 'This is the description',
+  name: 'Food Fight North London 2015',
+  group_description: 'Open Group for new members!  Come learn the ropes and meet some of the old hands.',
   creator_id: 1,
-  user_id: [1, 2, 3, 4, 5, 6, 7, 8],
+
   imageurl: "group1.jpg"
       )
 
 
 harrysgroup = Group.create!(
-  name: 'Harrys Awesome Group',
+  name: 'Come Dine with Men',
   group_description: 'This is the description',
   creator_id: 2,
-  user_id: [1, 2, 3],
+
   imageurl: "group.jpg"
 
       )
 
 zoesgroup = Group.create!(
-  name: 'Zoes Awesome Group',
-  group_description: 'This is the description',
+  name: 'Forbidden Meat Club',
+  group_description: 'Not your average Soylent Green.',
   creator_id: 3,
-  user_id: [2, 3],
+
   imageurl: "dinner.jpg"
 
       )
 
 # ........Tournaments ............
 
-alexstournament = Tournament.create! :creator_id => 1, :name => 'Dine with friends Alpha test', :tournament_description => 'This is the first ever test of the Dine with Friends App.  It will be a short and simple tournament, only rule: no ice-cream', :group_id => 1, :user_id => [1, 2, 3], :imageurl => "dinner-party2.jpg", :stake => 10
-alexstournament2 = Tournament.create! :creator_id => 1, :name => 'Dine with friends Beta test', :tournament_description => 'This is an beta test', :group_id => 1, :user_id => [1, 2, 3], :imageurl => "dinner2.jpg", :stake => 20
-alexstournament3 = Tournament.create! :creator_id => 1, :name => 'Dine with friends bugtesting test', :tournament_description => 'Bugfixing Party', :group_id => 1, :user_id => [1, 2, 3], :imageurl => "dinner3.jpg", :stake => 30
+alexstournament = Tournament.create! :creator_id => 1, :name => 'Dinner and Movie night', :tournament_description => 'Dinner and a movie, sounds great, except the audiance never shuts up and you are rated on both.', :imageurl => "dinner-party2.jpg", :stake => 10
+alexstournament2 = Tournament.create! :creator_id => 1, :name => 'Super Sunday', :tournament_description => 'Drunken Sunday Roasts.',  :imageurl => "dinner2.jpg", :stake => 20
+alexstournament3 = Tournament.create! :creator_id => 1, :name => 'Pizza Making', :tournament_description => 'Bugfixing Party',  :imageurl => "dinner3.jpg", :stake => 30
 
 
 # ........EVENTS...........
-alstournyround1 = Event.create! :imageurl => "http://rs1229.pbsrc.com/albums/ee473/jvlentini/TESTCARDS/bbc-hd-test-card.png~c200", :host_id => 1, :name => 'Dinner at Alexs house', :event_description => 'First round!  Gonna be great', :group_id => 1, :user_id => [1, 2, 3, 4, 5, 6, 7, 8], :tournament_id => 1, :location =>'Camden', :outcome => 5
-alstournyround2 = Event.create! :imageurl => "dinner3", :host_id => 2, :name => 'Dinner at Harrys house', :event_description => 'Harry house.  Its the same as mine...', :group_id => 1, :user_id => [1, 2, 3, 4, 5, 6, 7, 8], :tournament_id => 1, :location =>'Camden', :outcome => 5
-alstournyround3 = Event.create! :imageurl => "dinner4", :host_id => 3, :name => 'Dinner at Zoes house', :event_description => 'Final at zoes...', :group_id => 1, :user_id => [1, 2, 3, 4, 5, 6, 7, 8], :tournament_id => 1, :location =>'West London', :outcome => 7
-alstournyround4 = Event.create! :imageurl => "dinner5", :host_id => 3, :name => 'Dinner at Grouses house', :event_description => 'Grouse House', :group_id => 1, :user_id => [1, 2, 3, 4, 5, 6, 7, 8], :tournament_id => 1, :location =>'West London', :outcome => 7
-alstournyround5 = Event.create! :imageurl => "dinner6", :host_id => 3, :name => 'Dinner at Remes house', :event_description => 'Remes Flat', :group_id => 1, :user_id => [1, 2, 3, 4, 5, 6, 7, 8], :tournament_id => 1, :location =>'South London', :outcome => 7
+alstournyround1 = Event.create! :imageurl => "dinner1.jpg", :host_id => 1, :name => 'Dinner at Alexs house', :event_description => 'Guys so excited to be hosting first event!  Going to set the bar high so better be prepared!', :location =>'23 Elsworthy Road', :outcome => 5, :start_date => DateTime.parse("07/16/2015 8:00") 
+alstournyround2 = Event.create! :imageurl => "dinner3.jpg", :host_id => 2, :name => 'Dinner at Harrys house', :event_description => 'So I wasnt sure what to make or do, so Im falling back on a classic, Theme is jurassic park, you guys better dress up as im going to make all the insane food they had in the kitchen.' :tournament_id => 1, :location =>'23 Elsworthy Road', :outcome => 5, :start_date => DateTime.parse("07/17/2015 8:00") 
+alstournyround3 = Event.create! :imageurl => "dinner4.jpg", :host_id => 3, :name => 'Dinner at Zoes house', :event_description => 'Not sure what to say here so im going to think about it and update it later.', :location =>'West London', :outcome => 7, :start_date => DateTime.parse("07/18/2015 8:00") 
+alstournyround4 = Event.create! :imageurl => "dinner5.jpg", :host_id => 4, :name => 'Dinner at Eds house', :event_description => 'Not sure what to say here so im going to think about it and update it later.', :location =>'West London', :outcome => 7, :start_date => DateTime.parse("07/19/2015 8:00") 
+alstournyround5 = Event.create! :imageurl => "dinner6.jpg", :host_id => 5, :name => 'Dinner at Remes house', :event_description => 'I promise nothing except steak and whiskey.  Sam..will probably be Fetching and cook the steak and whiskey.', :location =>'South London', :outcome => 7, :start_date => DateTime.parse("07/20/2015 8:00") 
 
+alexsgroup.tournaments << alstournyround1 << alstournyround2 << alstournyround3 << alstournyround4 << alstournyround5
 
-alex.group_id << 1 << 2 << 3
-# Group id = what groups you are in - Wait is this redundant?
-alex.save
+alexsgroup.users << Users.all
+zoesgroup.users << alex << harry
+harrysgroup.users << alex << zoe
+
+alexstournament << Users.all
+
+alstournyround1.users << alex << harry << remes << zoe << grouse
+alstournyround2.users << alex << harry << remes << zoe << grouse
+alstournyround3.users << alex << harry << remes << zoe << grouse
+alstournyround4.users << alex << harry << remes << zoe << grouse
+alstournyround5.users << alex << harry << remes << zoe << grouse
+
 
 
 # .....................
